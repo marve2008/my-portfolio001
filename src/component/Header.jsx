bdb519e96ef339a237c64b4289409d034c36917c
marvellous <ejikemarvellous5@gmail.com>
2025-07-26 09:56:46 +0100

    second commit

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -4,14 +4,12 @@ import { FaSun, FaMoon } from 'react-icons/fa'
 
 const Header = ({ darkMode, toggleDarkMode }) => {
   return (
-    <div className='h-[80px] flex justify-start items-center gap-10'>
+    <div className='h-[80px] flex justify-between items-center gap-10'>
        <img src={pix} alt="pix"  className={`h-[60px] w-[80px] ${darkMode ? 'dark-mode-img' : ''}`}/>
-       <div className='flex gap-20'>
        <h3 className='text-2xl'>Marvey</h3>
         <button onClick={toggleDarkMode} className='p-2 rounded border border-gray-500'>
         {darkMode ? <FaMoon/> : <FaSun/>}
       </button>
-      </div>
     </div>
   )
 }
